220d914d4a4822413c4c26917eb74f4066165243
Dafne Coconi <daf.coconi@gmail.com>
2024-04-23 20:02:05 -0600

    run library

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from libGraph import mesh_graph, erdos_renyi_graph, gilbert_graph, geografico_simple_graph, barabasi_albert_graph, dorogovtsev_mendes_graph
 
-#mesh_graph(3,10, False)
+#mesh_graph(m = 3, n = 10, dirigido = False)
 #mesh_graph(10,10, False)
 #mesh_graph(10,50, False)
 
-#erdos_renyi_graph(30, 60, False)
+#erdos_renyi_graph(30, 35, False)
 #erdos_renyi_graph(100, 120, False)
 #erdos_renyi_graph(500, 520, False)
 
@@ -14,12 +14,12 @@ from libGraph import mesh_graph, erdos_renyi_graph, gilbert_graph, geografico_si
 
 #geografico_simple_graph(30, 0.3, False)
 #geografico_simple_graph(100, 0.3, False)
-#geografico_simple_graph(500, 0.3, False)
+#geografico_simple_graph(500, 0.15, False)
 
-#barabasi_albert_graph(30,2, False)
+barabasi_albert_graph(30,2, False)
 #barabasi_albert_graph(100,2, False)
 #barabasi_albert_graph(500,2, False)
 
-dorogovtsev_mendes_graph(30, False)
-dorogovtsev_mendes_graph(100, False)
+#dorogovtsev_mendes_graph(30, False)
+#dorogovtsev_mendes_graph(100, False)
 dorogovtsev_mendes_graph(500, False)
